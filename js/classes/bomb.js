8ec3ce500300f05d1bb98f27bad4402470bd7ce7
jv-obrabo <jv2404200717@gmail.com>
2024-05-23 10:26:52 -0400

    add  draw function

--- js/classes/bomb.js
+++ js/classes/bomb.js
@@ -7,8 +7,20 @@ class Bomb {
     this.color="";
     this.opacity= 1,
     this.active=false;
+
     gsap.to(this, {
     radius:30
     });
+   }
+
+   draw() {
+    c.save();
+    c.globalAlpha = this.opacity;
+    c.beginPath();
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2, false);
+    c.closePath();
+    c.fillStyle = this.color;
+    c.fill();
+    c.restore();
    }
 }
