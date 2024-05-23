407fd5cd84065de52b35bdea7d8854bcd287d8b1
jv-obrabo <jv2404200717@gmail.com>
2024-05-23 10:06:39 -0400

    add  Bomb class

---
+++ js/classes/bomb.js
@@ -0,0 +1,14 @@
+class Bomb {
+   static radius = 30;
+   constructor({ position,velocity,}) {
+    this.position=position;
+    this.velocity=velocity;
+    this.radius=0;
+    this.color="";
+    this.opacity= 1,
+    this.active=false;
+    gsap.to(this, {
+    radius:30
+    });
+   }
+}
