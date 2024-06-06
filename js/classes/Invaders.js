12024eb29ae61f93abdd105509f5d61a0d8eff10
jv-obrabo <jv2404200717@gmail.com>
2024-06-06 11:00:51 -0400

    add invaders.js class

---
+++ js/classes/Invaders.js
@@ -0,0 +1,32 @@
+class Invader {
+    constructor({position}) {
+        this.velocity = {
+            x: 0,
+            y: 0
+        };
+
+        const image = new Image();
+        image.src = "./images/invader.png";
+        image.onload = () => {
+            const scale = 1;
+            this.image = image;
+            this.width = image.width * scale;
+            this.height = image.height * scale;
+            this.position = {
+                x: position.x,
+                y: position.y
+            };
+         };
+      }
+
+      draw(c){
+        c.drawImage(
+            this.image,
+            this.position.x,
+            this.position.y,
+            this.width,
+            this.height
+        );
+      }
+}
+
