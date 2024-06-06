37e88a7b03448f5699c8cf301204ee4ed20dbb52
jv-obrabo <jv2404200717@gmail.com>
2024-06-06 11:43:27 -0400

    add  rectangularCollision  function in util.js file

--- js/util.js
+++ js/util.js
@@ -1,33 +1,35 @@
 function randomBetween(min, max) {
-    return Math.random() * (max - min + 1) + min; // Corrigido o parêntese extra
+  return Math.random() * (max - min + 1) + min; // Corrigido o parêntese extra
 }
 
-function createScoreLabel(score = 106, object) { // Corrigido o parêntese extra e o nome da variável "scorelabel" para "scoreLabel"
-    const scoreLabel = document.createElement("label"); // Corrigido o nome da variável de "scorelabel" para "scoreLabel"
-    scoreLabel.innerHTML = score;
-    scoreLabel.style.position = "absolute";
-    scoreLabel.style.color = "white";
-    scoreLabel.style.top = object.position.y + "px"; 
-    scoreLabel.style.left = object.position.x + "px";
-    scoreLabel.style.fontSize = "20px"; // Corrigido o nome da variável de "ScoreLabel" para "scoreLabel"
-    
-    document.querySelector("#parentDiv").appendChild(scoreLabel);
-    
-    gsap.to(scoreLabel, {
-        opacity: 0,
-        y: -30,
-        duration: 0.75,
-        onComplete: () => {
-            document.querySelector("#parentDiv").removeChild(scoreLabel);
-        }
-    });
+function createScoreLabel(score = 106, object) {
+  // Corrigido o parêntese extra e o nome da variável "scorelabel" para "scoreLabel"
+  const scoreLabel = document.createElement("label"); // Corrigido o nome da variável de "scorelabel" para "scoreLabel"
+  scoreLabel.innerHTML = score;
+  scoreLabel.style.position = "absolute";
+  scoreLabel.style.color = "white";
+  scoreLabel.style.left = object.position.x + "px";
+  scoreLabel.style.top = object.position.y + "px";
+  scoreLabel.style.userSelect = "none";
+
+  document.querySelector("#parentDiv").appendChild(scoreLabel);
+
+  gsap.to(scoreLabel, {
+    opacity: 0,
+    y: -30,
+    duration: 0.75,
+    onComplete: () => {
+      document.querySelector("#parentDiv").removeChild(scoreLabel);
+    }
+  });
 }
 
-function rectangularCollission({ rectangle1, rectangle2 }) { // Corrigido o nome da variável de "rectangularCollission" para "rectangularCollision"
-    return (
-        rectangle1.position.y + rectangle1.height >= rectangle2.position.y &&
-        rectangle1.position.x + rectangle1.width >= rectangle2.position.x &&
-        rectangle1.position.x <= rectangle2.position.x + rectangle2.width
-    );
-}
+function rectangularCollission({ rectangle1, rectangle2 }) {
+  // Corrigido o nome da variável de "rectangularCollission" para "rectangularCollision"
+  return (
+    rectangle1.position.y + rectangle1.height >= rectangle2.position.y &&
+    rectangle1.position.x + rectangle1.width >= rectangle2.position.x &&
+    rectangle1.position.x <= rectangle2.position.x + rectangle2.width
+  );
+ }
 
