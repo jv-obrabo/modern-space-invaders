ab332a010c7e513faf7fb284f29cab60ea926fc2
jv-obrabo <jv2404200717@gmail.com>
2024-06-06 11:00:19 -0400

    add invaderProjectlli.js class

---
+++ js/classes/InvaderProjectile.js
@@ -0,0 +1,19 @@
+class invaderProjectile {
+    constructor({ positiion, velocity }) {
+      this.positiion = positiion;
+      this.velocity = velocity;
+      this.width = 3;
+      this.height = 10;
+    }
+  
+    draw() {
+      c.fillStyle = "white";
+      c.fillRect(this.positiion.x, this.positiion.y, this.width, this.height);
+    }
+  
+    update() {
+      this.draw();
+      this.positiion.x += this.velocity.x;
+      this.positiion.y += this.velocity.y;
+    }
+  }
