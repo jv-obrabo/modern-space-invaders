57e8fe387779989aee6b5bec928118dcd9b138a7
jv-obrabo <jv2404200717@gmail.com>
2024-06-11 11:43:21 -0400

    add initial variables

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
-const scorEl = document.querySelector("#scoreEl");
+const scoreEl = document.querySelector("#scoreEl");
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
 
-canvas.width=1024;
+canvas.width = 1024;
 canvas.height = 576;
 
+let player = new Player();
+let projectiles = [];
+let grids = [];
+let invaderProjectiles = [];
+let particles = [];
+let bombs = [];
+let powerUps = [];
+
+let keys = {
+    ArrowLeft: {
+        pressed: false 
+    },
+    ArrowRight: {
+        pressed: false
+    },
+    space: {
+        pressed: false
+    }
+};
+
+let frames = 0;
+let randomInterval = Math.floor(Math.random() * 500 + 500);
+
+let gama = {
+    over: false,
+    active: true
+};
+
+let score = 0;
+
+let spawnBuffer = 500;
+let fps = 60;
+let fpsInterval = 1000 / fps;
+
+let msPrev = window.performance.now();
