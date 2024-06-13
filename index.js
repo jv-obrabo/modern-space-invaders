1d8a171ee94f71f69e83b2676d54d980d1e5c9b9
jv-obrabo <jv2404200717@gmail.com>
2024-06-13 08:28:37 -0400

    Initialized the creaion of the animate function

--- index.js
+++ index.js
@@ -108,3 +108,20 @@ function endGame() {
     fades: true
   });
 }
+
+function animate( ) {
+    if (!game.active) return;
+    requestAnimationFrame(animate);
+
+    const msNow =  window.performance.now();
+    const elapsed = msNow - msPrev;
+
+    if (elapsed > fpsInterval) {
+
+        msPrev = msNow - (elapsed % fpsInterval);
+        
+       c.fillStyle = "black";    
+       c.fillRect(0, 0, canvas.width, canvas.height);
+        
+     }
+}
