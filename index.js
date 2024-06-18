5ba899a075ba4b5bdbc8250c377d9be5d7893e7c
jv-obrabo <jv2404200717@gmail.com>
2024-06-18 11:26:25 -0400

    add grids update, position and status of invaders grids. Checks for colisions between invaders and player projectilies when a colision is detected, and ends the game if an attacker collides with the player.

--- index.js
+++ index.js
@@ -65,18 +65,19 @@ function init() {
   frames = 0;
   randomInterval = Math.floor(Math.random() * 500 + 500);
 
-  gama = {
+  game = {
     over: false,
     active: true
   };
 
   score = 0;
+
   for (let i = 0; i < 100; i++) {
     particles.push(
-      new particles({
+      new Particle({
         position: {
-          x: Math.floor(Math.random() * canvas.width),
-          y: Math.floor(Math.random() * canvas.height)
+          x: Math.random() * canvas.width,
+          y: Math.random() * canvas.height
         },
         velocity: {
           x: 0,
@@ -90,20 +91,20 @@ function init() {
 }
 
 function endGame() {
-  Audio.gameOver.play();
+  audio.gameOver.play();
 
   setTimeout(() => {
-    player.opacity = 0;
-    gameOver = true;
+    players.opacity = 0;
+    game.over = true;
   }, 0);
 
   setTimeout(() => {
     game.active = false;
-    document.querySelector("restartScreen").style.display = "flex";
+    document.querySelector("#restartScreen").computedStyleMap.display = "flex";
   }, 2000);
 
   createParticles({
-    Object: player,
+    object: player,
     color: "white",
     fades: true
   });
@@ -116,12 +117,13 @@ function animate() {
   const msNow = window.performance.now();
   const elapsed = msNow - msPrev;
 
-  if (elapsed > fpsInterval) {
-    msPrev = msNow - (elapsed % fpsInterval);
+  if (elapsed < fpsInterval) return;
 
-    c.fillStyle = "black";
-    c.fillRect(0, 0, canvas.width, canvas.height);
-  }
+  msPrev = msNow - (elapsed % fpsInterval);
+
+  c.fillStyle = "black";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+
   for (let i = powerUps.length - 1; i >= 0; i--) {
     const powerUp = powerUps[i];
 
@@ -129,7 +131,6 @@ function animate() {
       powerUps.splice(i, 1);
     else powerUp.update();
   }
-
   if (frames % 500 === 0) {
     powerUps.push(
       new PowerUp({
@@ -144,4 +145,222 @@ function animate() {
       })
     );
   }
+
+  if (frames % 200 === 0 && bombs.length < 3) {
+    bombs.push(
+      new Bomb({
+        position: {
+          x: randomBetween(Bomb.radius, canvas.width - bomb.radius),
+          y: randomBetween(Bomb.radius, canvas.height - Bomb.radius)
+        },
+        velocity: {
+          x: (Math.random() - 0.5) * 6,
+          y: (Math.random() - 0.5) * 6
+        }
+      })
+    );
+  }
+
+  for (let i = bombs.length - 1; i >= 0; i--) {
+    const bomb = bombs[i];
+
+    if (bomb.opacity <= 0) {
+      bombs.splice(i, 1);
+    } else bomb.update();
+  }
+
+  player.update();
+
+  for (let i = player.particles.length - 1; i >= 0; i--) {
+    const particle = player.particles[i];
+    particle.update();
+    if (particle.opacity === 0) player.particles[1].splice(i, 1);
+  }
+
+  particles.forEach((particle, i) => {
+    if (particle.position.y - particle.radius >= canvas.height) {
+      particles.position.x = Math.random() * canvas.width;
+      particles.position.y = -particle.radius;
+    }
+
+    if (particle.opacity <= 0) {
+      setTimeout(() => {
+        particles.splice(i, 1);
+      }, 0);
+    } else {
+      particle.update();
+    }
+  });
+
+  invaderProjectiles.forEach((invaderProjectile, index) => {
+    if (
+      invaderProjectile.position.y + invaderProjectile.height >=
+      canvas.height
+    ) {
+      setTimeout(() => {
+        invaderProjectiles.splice(index, 1);
+      }, 0);
+    } else invaderProjectile.update();
+
+    if (
+      rectangularCollision({
+        rectangle1: invaderProjectile,
+        rectangle2: player
+      })
+    ) {
+      invaderProjectiles.splice(index, 1);
+      endGame();
+    }
+  });
+
+  for (let i = projectiles - 1; i >= 0; i--) {
+    const projectile = projectiles[i];
+
+    for (let j = bombs.length - 1; j >= 0; j--) {
+      const bomb = bombs[j];
+
+      if (
+        Math.hypot(
+          projectile.position.x - bomb.position.x,
+          projectile.position.y - bomb.position.y
+        ) <
+          projectile.radius + bomb.radius &&
+        !bomb.active
+      ) {
+        projectiles.splice(i, 1);
+        bombs.explode();
+      }
+    }
+
+    for (let j = powerUps.length - 1; j >= 0; j--) {
+      const powerUp = powerUps[j];
+
+      if (
+        Math.hypot(
+          projectile.position.x - powerUp.position.x,
+          projectile.position.y - powerUp.position.y
+        ) <
+        projectile.radius + powerUp.radius
+      ) {
+        projectiles.splice(i, 1);
+        powerUps.splice(j, 1);
+        player.powerUp = "Metralhadora";
+        audio.bonus.play();
+
+        setTimeout(() => {
+          player.powerUp = null;
+        }, 5000);
+      }
+    }
+
+    if (projectile.position.y + particles.radius <= 0) {
+      projectiles.splice(i, 1);
+    } else {
+      projectile.update();
+    }
+  }
+
+  grids.forEach((grid, gridIndex) => {
+    grid.update();
+    if (frames % 100 === 0 && grid.invaders.length > 0) {
+      grid.invaders[Math.floor(Math.random() * grid.invaders.length)].shoot(
+        invadersProjectiles
+      );
+    }
+
+    for (let i = grid.invaders.length - 1; i >= 0; i--) {
+      const invader = grid.invaders[i];
+      invader.update({ velocity: grid.velocity });
+
+      for (let j = bombs.length - 1; j >= 0; j--) {
+        const bomb = bombs[j];
+        const invaderRadius = 15;
+
+        if (
+          Math.hypot(
+            invader.position.x - bomb.position.x,
+            invader.position.y - bomb.position.y
+          ) <
+            invaderRadius + bomb.radius &&
+          bomb.active
+        ) {
+          score += 50;
+          scoreEl.innerHTML = score;
+
+          grid.invaders.splice(i, 1);
+          createScorelabel({
+            object: invader,
+            score: 50
+          });
+
+          createParticles({
+            object: invader,
+            fades: true
+          });
+        }
+      }
+
+      projectiles.forEach((projectiles, j) => {
+        if (
+          projectiles.position.y - projectiles.radius <=
+            invader.position.y + invader.height &&
+          projectiles.position.x + projectile.radius >= invader.position.x &&
+          projectiles.position.x - projectile.radius <=
+            invader.position.x + invader.width &&
+          projectiles.position.y + projectiles.radius >= invader.position.y
+        ) {
+          setTimeout(() => {
+            const invaderFound = grid.invaders.find(
+              (invader2) => invader2 === invader
+            );
+
+            const projectilefound = projectiles.find(
+              (projectile2) => projectile2 === projectile2
+            );
+
+            if (invaderFound && projectilefound) {
+              score += 100;
+              scoreEl.innerHTML = score;
+
+              createScoreLabel({
+                object: invader
+              });
+
+              createParticles({
+                object: invader,
+                fades: true
+              });
+
+              audio.explode.play();
+              grid.invaders.splice(i, 1);
+              projectiles.splice(j, 1);
+
+              if (grid.invaders.length > 0) {
+                const firstInvaders = grid.invaders[0];
+                const lastInvaders = grid.invaders[grid.invaders.length - 1];
+
+                grid.width =
+                  lastInvaders.position.x -
+                  firstInvaders.position.x +
+                  lastInvaders.width;
+
+                grid.position.x = firstInvader.position.x;
+              } else {
+                grids.splice(gridIndex, 1);
+              }
+            }
+          }, 0);
+        }
+      });
+
+      if (
+        rectangularCollision({
+          rectangle1: invader,
+          rectangle2: player
+        }) &&
+        !game.over
+      )
+        endGame();
+    }
+  });
 }
