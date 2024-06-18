bfe4fca12f10f255bbd0b909a8757eb2ac1d838d
jv-obrabo <jv2404200717@gmail.com>
2024-06-18 11:46:06 -0400

    add player movement control, updates player speed  and rotation based on ArrowLeft an ArrowRight key  presses

--- index.js
+++ index.js
@@ -363,4 +363,18 @@ function animate() {
         endGame();
     }
   });
+
+  if(keys.ArrowLeft.pressed && player.position.x >= 0) {
+    player.position.x = -7;
+    player.rotation = -0.15;
+  } else if(
+    keys.ArrowRight.pressed &&
+    player.position.x + player.width <= canvas.width
+  ) {
+    player.position.x = 7;
+    player.rotation = 0.15;
+  } else {
+    player.position.x = 0;
+    player.rotation = 0;
+  }
 }
